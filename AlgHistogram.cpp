d0816f745d0d32741fed0516310063462e28fa33
feniks65 <feniks65@users.noreply.github.com>
2016-04-28 09:24:31 +0200

    body of AlgHistogram.cpp

---
+++ AlgHistogram.cpp
@@ -0,0 +1,36 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <vector>
+#include <iostream>
+
+#include "Tester.hpp"
+#include "Preparator.hpp"
+#include "HoughTransform.hpp"
+
+using namespace std;
+using namespace cv;
+
+Mat AlgHistogram::rotate(Mat inputImage)
+{
+  Mat outputImage;
+  
+  
+  return outputImage;
+}
+
+Mat AlgHistogram::drawChart(Mat inputImage)
+{
+  Mat outputImage;
+  
+  
+  return outputImage;
+}
+
+Mat AlgHistogram::transform(Mat inputImage)
+{
+  Mat outputImage;
+  
+  
+  return outputImage;
+}
